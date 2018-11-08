ac0de623b0031ed6549bc64f616095e3de8bbe93
brandon <dabmajor@gmail.com>
2018-11-08 11:28:28 -0700

    test code version of bank ms system

--- bank_account_management.py
+++ bank_account_management.py
@@ -1,4 +1,51 @@
-# 
+#
 # main program file for fictitious bank account management system
 #
 
+BANK_NAME = "Major Bank"
+
+
+def main():
+    welcome()
+    myaccount = Account("Brandon", 1000)
+    print(myaccount.balance, myaccount.owner)
+    myaccount.deposit(500)
+    myaccount.withdrawal(250)
+    myaccount.withdrawal(myaccount.balance)
+    myaccount.withdrawal(1)
+
+
+# object class for each bank account in the system
+class Account:
+
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print("\nnew balance is ${}".format(self.balance))
+
+    def withdrawal(self,amount):
+        if amount <= self.balance:
+            # process the withdrawal
+            self.balance -= amount
+            print("\nnew balance is ${}".format(self.balance))
+        else:
+            print("\ninsufficient funds in account")
+            print("\ncurrent balance is ${}".format(self.balance))
+
+
+class Member:
+    pass
+
+
+def welcome():
+    welcome_string = "*  Welcome to {}'s account management system. *".format(BANK_NAME)
+    print("*" * len(welcome_string))
+    print(welcome_string)
+    print("*" * len(welcome_string))
+
+
+main()
+exit()
